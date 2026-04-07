06f812b524708c04b4a5235bf858461895aee875
Mirxonjon <mirxonjonismanov152@gmail.com>
2026-04-07 10:41:54 +0500

    custom LMS (uzbek + branding)

--- docker/init.sh
+++ docker/init.sh
@@ -1,40 +1,50 @@
-#!bin/bash
+#!/bin/bash
+
+set -e
 
 if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     echo "Bench already exists, skipping init"
-    cd frappe-bench
+    cd /home/frappe/frappe-bench
     bench start
+    exit 0
 else
     echo "Creating new bench..."
 fi
 
 export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
 
+cd /home/frappe
+
 bench init --skip-redis-config-generation frappe-bench
 
-cd frappe-bench
+cd /home/frappe/frappe-bench
 
-# Use containers instead of localhost
+# DB va Redis sozlash
 bench set-mariadb-host mariadb
 bench set-redis-cache-host redis://redis:6379
 bench set-redis-queue-host redis://redis:6379
 bench set-redis-socketio-host redis://redis:6379
 
-# Remove redis, watch from Procfile
+# Procfile tozalash
 sed -i '/redis/d' ./Procfile
 sed -i '/watch/d' ./Procfile
 
+# Apps
 bench get-app payments
 bench get-app lms
 
+# Site yaratish
 bench new-site lms.localhost \
 --force \
 --mariadb-root-password 123 \
 --admin-password admin \
 --no-mariadb-socket
 
+# Install apps
 bench --site lms.localhost install-app payments
 bench --site lms.localhost install-app lms
+
+# Dev mode
 bench --site lms.localhost set-config developer_mode 1
 bench --site lms.localhost clear-cache
 bench use lms.localhost
